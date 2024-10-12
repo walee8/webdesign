700d4ebd7c0b9f2a10bf1bc305adc7fed076f4c1
walee8 <94443694+walee8@users.noreply.github.com>
2024-10-12 21:26:53 +0600

    Add files via upload

---
+++ Tourist/js/custom.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
